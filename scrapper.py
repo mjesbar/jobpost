246bea757dfc2fefb7b3044fb1288db96bb5adba
mjesbar <miguedjba96@gmail.com>
2023-02-17 17:10:40 -0500

    +script: testing chromedriver

---
+++ scrapper.py
@@ -0,0 +1,22 @@
+from selenium import  webdriver
+from selenium.webdriver import Chrome, ChromeOptions
+
+
+chrome_options = ChromeOptions()
+chrome_options.page_load_strategy = 'eager'
+chrome_options.add_argument('--incognito')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--window-position=960,0')
+chrome_options.add_argument('--window-size=960,700')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-extensions')
+
+browser = webdriver.Chrome(options=chrome_options)
+browser.get('https://www.ldoceonline.com/')
+
+print(browser.current_url)
+
+browser.quit()
+
+
+

05ace1aa1430830448edcc02b0a1d433a692741b
mjesbar <miguedjba96@gmail.com>
2023-02-18 16:09:56 -0500

    ~

--- scrapper.py
+++ scrapper.py
@@ -4,7 +4,7 @@ from selenium.webdriver import Chrome, ChromeOptions
 
 chrome_options = ChromeOptions()
 chrome_options.page_load_strategy = 'eager'
-chrome_options.add_argument('--incognito')
+chrome_options.add_argument('--start-in-incognito')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--window-position=960,0')
 chrome_options.add_argument('--window-size=960,700')
@@ -15,6 +15,7 @@ browser = webdriver.Chrome(options=chrome_options)
 browser.get('https://www.ldoceonline.com/')
 
 print(browser.current_url)
+print(browser.title)
 
 browser.quit()
 

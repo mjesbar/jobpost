a1ad9da39a3082ca6472e9148535411db5a19495
mjesbar <miguedjba96@gmail.com>
2023-02-21 16:55:59 -0500

    +script: add new diagnostic file test.py

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
-import pandas
+import pandas, os, datetime
+print()
 
-partitions = 
 
-parquet = pandas.read_parquet('data/data2023-02-21.parquet.gz',
-                              'pyarrow')
+data_dir = os.listdir('data/')
+old_partitions = list()
+new_partition = str()
+today = datetime.date.today()
 
-print(parquet['id'])
-print(parquet['id'].drop_duplicates().count())
-print(parquet['id'].count())
+for file in data_dir:
+    if ('csv' in file) & (f'{today}' in file):
+        new_partition = file
+        print("new partition:\n\t", new_partition)
+        print("-------------\nold_partitions:")
+    if ('csv' in file) & (f'{today}' not in file):
+        old_partitions.append(file)
+        print("\t",old_partitions[-1])
 

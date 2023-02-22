1cec018d9cba9fff17e05fa40f06859b90d6744d
mjesbar <miguedjba96@gmail.com>
2023-02-22 14:51:20 -0500

    -+script: test.py renamed

--- descriptor.py
+++ descriptor.py
@@ -12,7 +12,9 @@ for file in data_dir:
     if ('csv' in file) & (f'{today}' in file):
         new_partition = file
         print("new partition:\n\t", new_partition)
-        print("-------------\nold_partitions:")
+        print("==============================================\nold_partitions:")
+
+for file in data_dir:
     if ('csv' in file) & (f'{today}' not in file):
         old_partitions.append(file)
         print("\t",old_partitions[-1])

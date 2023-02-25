9a8cd37544aa66e08b287f1aacc74c0466fe32b6
mjesbar <miguedjba96@gmail.com>
2023-02-25 13:26:30 -0500

    ~+script: starting lambda-cleaner feature

---
+++ lambda/function.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3.9
+
+import pandas
+import json
+
--- uploader.py
+++ uploader.py
@@ -1,12 +1,17 @@
+from re import A
 import pandas, os, datetime, json
 import boto3
+from pandas.core.dtypes.common import classes
 print()
 
 
 def describe(ls:list):
-
+    """
+    fDescription:
+    list the batches that we've already saved from scraping.
+    """
     partition = str()
-
+ 
     for file in data_dir:
         if ('parquet' in file) & (f'{today}' in file):
             partition = file
@@ -21,8 +26,12 @@ def describe(ls:list):
     return partition
 
 
+
 def upload_s3():
-    
+    """
+    fDescription: 
+    upload using the low-level API function from boto3 with a S3 client.
+    """    
     s3 = boto3.client('s3')
     new_partition_binary = open(f"data/{new_partition}", 'rb')
 
@@ -33,19 +42,33 @@ def upload_s3():
     return response
 
 
+
+class StatusCodeError(Exception):
+    def __init__(self, message) -> None:
+        self.message = "the opoeration has returned an error status code"
+        super().__init__(self.message)
+
+
+
+# main program execution scope ----------------------------------------------------------------------------------------------
+
 if __name__ == "__main__":
     
     today = datetime.date.today()
     old_partitions = list()
     new_partition = str()
-    data_dir = os.listdir('data/')
+    data_dir = os.listdir("./data")
 
     print("Describing file structure collected ... ")
     new_partition = describe(data_dir)
     print()
 
-    print("Uploading today's file to s3://jobpost-project/raw/ S3 bucket ... ")
-    response_object = upload_s3()
-    print("StatusCode:", response_object['ResponseMetadata']['HTTPStatusCode'])
-    print("partition object uploaded:", new_partition)
+    print("Uploading today's file to s3://jobpost-project/raw/ S3 bucket ... ", end='')
+    response = upload_s3()
+    response_status = response['ResponseMetadata']['HTTPStatusCode']
+    if (response_status == 200):
+        print(" -> ", response_status)
+    else:
+        raise StatusCodeError(" -> StatusCodeError")
+    print("PartitionObject just uploaded:", new_partition)
 

6cec44c755c21f3afcb2e9153283471ba96b41d9
mjesbar <miguedjba96@gmail.com>
2023-03-02 15:24:53 -0500

    +~code: ETL on posters df done

--- lambda/function.py
+++ lambda/function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.9
 from datetime import datetime
+from itertools import count
 import boto3, module, pandas
-from numpy import NaN, column_stack, nan
 import os, io, json, base64, re
 print()
 
@@ -30,41 +30,37 @@ def lambda_handler(event, context):
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
     # getting list the 'raw/' in 'jobpost-project' bucket
-    #s3client = boto3.client("s3")
-    #response = s3client.list_objects(Bucket="jobpost-project", Marker=raw_folder_key)
-    #response_content = response.get("Contents")
+    s3client = boto3.client("s3")
+    response = s3client.list_objects(Bucket="jobpost-project", Marker=raw_folder_key)
+    response_content = response.get("Contents")
 	# getting the file names
-    #for element in response_content:
-        #keys.append(element.get("Key"))
+    for element in response_content:
+       keys.append(element.get("Key"))
     # merging all the parquet partitions on bucket
-    #dataframe = module.merge_df(dataframe, keys, s3client, bucket_name)
+    dataframe = module.merge_df(dataframe, keys, s3client, bucket_name)
 
 #tmp test
-    ls_data = os.listdir("../data/")
-    ls_data_filtered = filter(lambda x: 'parquet' in x, ls_data)
-    ls = list(ls_data_filtered)
+    #ls_data = os.listdir("../data/")
+    #ls_data_filtered = filter(lambda x: 'parquet' in x, ls_data)
+    #ls = list(ls_data_filtered)
 
-    for file in ls:
-        tmp = f"../data/{file}"
-        tmpdf = pandas.read_parquet(tmp)
-        dataframe = pandas.concat([dataframe, tmpdf], ignore_index=True, )
+    #for file in ls:
+        #tmp = f"../data/{file}"
+        #tmpdf = pandas.read_parquet(tmp)
+        #dataframe = pandas.concat([dataframe, tmpdf], ignore_index=True, )
     
     print("Rows collected", dataframe.shape[0])
 
     # Tranformation
     # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-    
+
     # dropping duplicated post ids
     dataframe.drop_duplicates(subset='id', inplace=True)
+
     # filling None values
-    dataframe['type'].fillna('', inplace=True)
-    dataframe['company'].fillna('', inplace=True)
-    dataframe['experience'].fillna('', inplace=True)
-    # casting the salary to integer
-    dataframe['salary'] = dataframe['salary'].str.replace("$ ","", regex=False)
-    dataframe['salary'] = dataframe['salary'].str.replace(",00 (Mensual)","", regex=False)
-    dataframe['salary'] = dataframe['salary'].str.replace(".","", regex=False)
-    dataframe['salary'].fillna('', inplace=True)
+    dataframe['type'].fillna(pandas.NA, inplace=True)
+    dataframe['company'].fillna('anon', inplace=True)
+
     # casting education
     dataframe['description'] = dataframe['description'].str.decode('base64_codec')
     dataframe['description'] = dataframe['description'].str.decode('utf_8')
@@ -75,6 +71,7 @@ def lambda_handler(event, context):
     for (idx, token) in enumerate(tokens):
         dataframe['education'].mask(
             dataframe['education'].str.match(fr".*{token}.*"), token, inplace=True)
+    dataframe['education'].fillna(pandas.NA, inplace=True)
     mask_profesional = dataframe['description'].str.match(r".*(especialista+|profesional|conocimientos+|sólidos+).*")
     mask_bachilller = dataframe['description'].str.match(r".*(bachiller+|con+.?o?.?sin+.experiencia+).*")
     dataframe['education'].mask(
@@ -83,13 +80,24 @@ def lambda_handler(event, context):
         (mask_bachilller) & (dataframe['education']==''), tokens[1], inplace=True)
     dataframe['education'].mask(
         dataframe['education']=='', tokens[0], inplace=True)
+    
+    # casting the salary to integer
+    dataframe['salary'] = dataframe['salary'].str.replace("$ ","", regex=False)
+    dataframe['salary'] = dataframe['salary'].str.replace(",00 (Mensual)","", regex=False)
+    dataframe['salary'] = dataframe['salary'].str.replace(".","", regex=False)
+    mask_salary = dataframe['description'].str.extract(r".*\$.?(\d+[\.|\']\d{3}\.\d{3}) .*").squeeze()
+    mask_salary = mask_salary.str.replace('$','').str.replace('.','').str.replace('\'','').str.strip()
+    dataframe['salary'].update(mask_salary)
+    dataframe['salary'].fillna('0', inplace=True)
+
     # cleaning age
     dataframe['age'].mask(
         dataframe['age'].notna(), "specific", inplace=True)
     dataframe['age'].fillna('non-specific', inplace=True)
+
     # cleaning type
-    mask_type_hybrid1 = dataframe['description'].str.match(r".*(hibrido+|[p|P]resencial y remoto+).*")
-    mask_type_hybrid2 = dataframe['type'].str.match(r"^([h|H]ibrido+|[p|P]resencial y remoto+).*")
+    mask_type_hybrid1 = dataframe['description'].str.match(r".*(hibrido|[p|P]resencial y remoto)+.*")
+    mask_type_hybrid2 = dataframe['type'].str.match(r"^([h|H]ibrido|[p|P]resencial y remoto)+.*")
     mask_type_hybrid = mask_type_hybrid1 | mask_type_hybrid2
     mask_type_remote1 = dataframe['description'].str.match(r".*(remoto+).*")
     mask_type_remote2 = dataframe['type'].str.match(r"^([r|R]emoto+).*")
@@ -100,28 +108,44 @@ def lambda_handler(event, context):
         mask_type_hybrid, "hybrid", inplace=True)
     dataframe['type'].mask(
         ~(mask_type_hybrid|mask_type_remote) , "non-remote", inplace=True)
+    
     # cleaning experience
-    mask_experience1 = dataframe['experience'].str.extract(r"(\d)+ año").squeeze()
-    mask_experience2 = dataframe['description'].str.extract(r"(\d)+ año").squeeze()
+    mask_experience1 = dataframe['experience'].str.extract(r"(\d)+ año.?").squeeze()
+    mask_experience2 = dataframe['description'].str.extract(r"(\d)+.año.?").squeeze()
     dataframe['experience'].update(mask_experience1)
     dataframe['experience'].update(mask_experience2)
+    dataframe['experience'].fillna('0', inplace=True)
+
+    # tagging english skill
+    dataframe['english'] = dataframe['description'].str.match(
+        r".*(advanced|english|ingl[e|é]s|experience|level|work|knowledge|team|skill.?)+.*")
+    dataframe['english'].mask(
+        dataframe['english']==True, 'required', inplace=True)
+    dataframe['english'].mask(
+        dataframe['english']==False, 'non-required', inplace=True)
+    
     # dropping useless columns
-    dataframe.drop(columns=['title','postDate'], inplace=True)
+    posters = dataframe.drop(columns=['title','postDate'])
+    del dataframe
 
+    # encoding back the 'description' columns into base64
+    description_base64_index = posters['description'].index
+    description_base64 = list(posters['description'])
+    description_base64 = map(lambda x: x.encode('utf_8'), description_base64)
+    description_base64 = map(lambda x: base64.b64encode(x), description_base64)
+    posters['description'] = pandas.Series(data=list(description_base64),
+                                           index=description_base64_index)
 
+    # converting column dtypes from prior schema
+    schema = json.load(open("./schema.json", "r"))
+    posters = posters.astype(schema.get("posters"))
+    
+    # exchanging the order of 'english' and 'description' columns
+    posters[['description','english']] = posters[['english','description']]
+    posters.rename(columns={"description": "english", "english": "description"}, inplace=True)
 
-    print(dataframe.info())
-    print(dataframe)
-
-    # converting column types
-    #schema = json.load(open("./schema.json", "r"))
-    #dataframe = dataframe.astype(schema)
-    #print(dataframe.dtypes)
-
-
-
-
-
+    print("Rows after ETL", posters.shape[0])
+    print(posters.info())
 
 
     #module.map_df(dataframe, 'description', languages_df.columns)
@@ -129,7 +153,7 @@ def lambda_handler(event, context):
     # Loads - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
     # uploading tranformed 'dataframe'
-    #upload_response = module.upload_df(dataframe, s3client, bucket_name, target_folder_key)
+    upload_response = module.upload_df(posters, s3client, bucket_name, target_folder_key)
 
 
 
